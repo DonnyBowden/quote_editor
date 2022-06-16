1b35064d4b9fc426f014448c8a22b0a616c63806
Donny Bowden <donnell@entretechno.com>
2022-06-15 19:44:22 -0700

    Add line items model layer

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -11,8 +11,8 @@ class LineItemDatesController < ApplicationController
 
     if @line_item_date.save
       respond_to do |format|
-        format.html { redirect_to quote_path(@quote), notice: "Date was successfully created." }
-        format.turbo_stream { flash.now[:notice] = "Date was successfully created." }
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully created 1." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully created 2." }
       end
     else
       render :new, status: :unprocessable_entity
@@ -24,7 +24,10 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -33,7 +36,14 @@ class LineItemDatesController < ApplicationController
   def destroy
     @line_item_date.destroy
 
-    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+      redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+    enddef destroy
+    @line_item_date.destroy
+
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed." }
+    end
   end
 
   private
