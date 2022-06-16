3cff0263f24ad848b0ec89a88fdf47b672296152
Donny Bowden <donnell@entretechno.com>
2022-06-15 22:38:40 -0700

    line Items dates rails system test update 2

--- test/system/line_item_dates_test.rb
+++ test/system/line_item_dates_test.rb
@@ -1,63 +1,50 @@
 require "application_system_test_case"
 
-class LineItemSystemTest < ApplicationSystemTestCase
-  include ActionView::Helpers::NumberHelper
-
+class LineItemDatesTest < ApplicationSystemTestCase
   setup do
     login_as users(:accountant)
 
     @quote          = quotes(:first)
     @line_item_date = line_item_dates(:today)
-    @line_item      = line_items(:room_today)
 
     visit quote_path(@quote)
   end
 
-  test "Creating a new line item" do
+  test "Creating a new line item date" do
     assert_selector "h1", text: "First quote"
 
-    within "##{dom_id(@line_item_date)}" do
-      click_on "Add item", match: :first
-    end
+    click_on "New date"
     assert_selector "h1", text: "First quote"
+    fill_in "Date", with: Date.current + 1.day
 
-    fill_in "Name", with: "Animation"
-    fill_in "Quantity", with: 1
-    fill_in "Unit price", with: 1234
-    click_on "Create item"
-
-    assert_selector "h1", text: "First quote"
-    assert_text "Animation"
-    assert_text number_to_currency(1234)
+    click_on "Create date"
+    assert_text I18n.l(Date.current + 1.day, format: :long)
   end
 
-  test "Updating a line item" do
+  test "Updating a line item date" do
     assert_selector "h1", text: "First quote"
-
-    within "##{dom_id(@line_item)}" do
+  
+    within id: dom_id(@line_item_date, :edit) do
       click_on "Edit"
     end
+  
     assert_selector "h1", text: "First quote"
-
-    fill_in "Name", with: "Capybara article"
-    fill_in "Unit price", with: 1234
-    click_on "Update item"
-
-    assert_text "Capybara article"
-    assert_text number_to_currency(1234)
+  
+    fill_in "Date", with: Date.current + 1.day
+    click_on "Update date"
+  
+    assert_text I18n.l(Date.current + 1.day, format: :long)
   end
-
-  test "Destroying a line item" do
-    within "##{dom_id(@line_item_date)}" do
-      assert_text @line_item.name
+  
+  test "Destroying a line item date" do
+    assert_text I18n.l(Date.current, format: :long)
+  
+    accept_confirm do
+      within id: dom_id(@line_item_date, :edit) do
+        click_on "Delete"
+      end
     end
-
-    within "##{dom_id(@line_item)}" do
-      click_on "Delete"
-    end
-
-    within "##{dom_id(@line_item_date)}" do
-      assert_no_text @line_item.name
-    end
+  
+    assert_no_text I18n.l(Date.current, format: :long)
   end
 end
